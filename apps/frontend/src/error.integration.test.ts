376b6ee2d5b10e29e15b6e6ea0c91a4860e87216
ziimakc <ziimakc@gmail.com>
2025-05-07 16:21:07 +0300

    issue repro

--- apps/frontend/src/error.integration.test.ts
+++ apps/frontend/src/error.integration.test.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@libs/orm";
 import { expect, test } from "@playwright/test";
 
 test("should edit user", async ({ context, browser, page }) => {
-	const user = new PrismaClient();
+	console.log(PrismaClient)
 
 	await expect(1).toBe(1);
 });
